912f040f6db20dda4b1a9efd725eb72fb43e51d1
dpvpp <dpvpp22@yahoo.com>
2021-04-15 12:55:49 -0400

    home page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import greed from './images/greed.png'
+import glory from './images/glory.png'
 
 function App() {
   return (
     <div>
-      <img src={require('Greed.PNG')} alt="Greed.PNG" class="rounded float-end"></img>
+      <div class="container">
+        <div class="text-center">
+          <div class="row">
+            <div class="col-sm">
+              <h1>Dollars Bank ATM</h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">
+              <img alt="Greed" src={greed} class="rounded float-end"/>
+            </div>
+            <div class="col-sm">
+              <form>
+                <label for="pin">Enter Pin: </label><br/>
+                <input type="password" id="pin" name="pin" maxlength="4" /><br/>
+                <input type="submit" class="btn btn-primary" value="Enter"/>
+              </form> 
+            </div>
+            <div class="col-sm">
+              <img alt="Glory" src={glory} class="rounded float-start"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
