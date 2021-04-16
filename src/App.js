f481d72528c4c4637df26f128f6524fe181eecd5
dpvpp <dpvpp22@yahoo.com>
2021-04-16 16:47:47 -0400

    Menu switching

--- src/App.js
+++ src/App.js
@@ -2,30 +2,69 @@ import './App.css';
 import greed from './images/greed.png';
 import glory from './images/glory.png';
 import GetPin from './components/GetPin';
+import { useState } from 'react';
+import Account from './classes/Account'
+import AccountOptions from './components/AccountOptions'
 
 function App() {
+  let accounts = new Map();
+  const[pin,setPin] = useState(0);
+  const[menu, setMenu] = useState(0)
+
+  const addAccounts = () => {
+    accounts.set('1234', new Account('1234', 1234))
+    accounts.set('1111', new Account('1111', 1111))
+    accounts.set('1122', new Account('1122', 1232))
+    accounts.set('3333', new Account('3333', 13234))
+    accounts.set('4444', new Account('4444', 4444))
+    accounts.set('5555', new Account('5555', 5555))
+  }
+
+  const submitPin = () => {
+    if(accounts.get(pin) != null) {
+      setMenu(1);
+    }
+    else {
+      getMenu();
+    }
+  }
+
+  const getMenu = () => {
+    switch(menu) {
+      case 0 :
+        return(
+          <GetPin setPin={setPin} submitPin={submitPin}/>
+        );
+      case 1 :
+        return(
+          <AccountOptions />
+        );
+      default :
+        return (
+          <h1>Some strange error has occurred</h1>
+        )
+    }
+  }
+
   return (
     <div>
-      <div class="container">
+        {addAccounts()}
         <div class="text-center">
-          <div class="row">
-            <div class="col">
-              <h1 style={{color:'white',backgroundColor: 'green'}}>Dollars Bank ATM</h1>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <img alt="Greed" src={greed} class="img-fluid rounded float-end"/>
-            </div>
-            <div class="col">
-              <GetPin />
-            </div>
-            <div class="col">
-              <img alt="Glory" src={glory} class="img-fluid rounded float-start"/>
+          <h1 style={{color:'white',backgroundColor: 'green'}}>Dollars Bank ATM</h1>
+          <div class="container">  
+            <div class="row">
+              <div class="col">
+                <img alt="Greed" src={greed} class="img-fluid rounded float-end"/>
+              </div>
+              <div class="col">
+                {getMenu()}
+              </div>
+              <div class="col">
+                <img alt="Glory" src={glory} class="img-fluid rounded float-start"/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
   );
 }
