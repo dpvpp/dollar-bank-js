07db3b218766d2c5963fb433b5da461bb79a929e
dpvpp <dpvpp22@yahoo.com>
2021-04-18 15:54:33 -0400

    Now it's ready

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
     }
     else {
       let update = {pin: account.pin, balance: account.balance, transactions: account.transactions};
-      update.balance = parseFloat((update.balance) - parseFloat(amount)).toFixed(2);
+      update.balance = (parseFloat(update.balance) - parseFloat(amount)).toFixed(2);
       update.transactions.push("Withdrawal: $" + amount + " " + new Date())
       
       setAccount(update);
@@ -93,7 +93,7 @@ function App() {
   const submitDeposit = (amount) => {
 
     let update = {pin: account.pin, balance: account.balance, transactions: account.transactions};
-    update.balance = parseFloat((update.balance) + parseFloat(amount)).toFixed(2);
+    update.balance = (parseFloat(update.balance) + parseFloat(amount)).toFixed(2);
     update.balance.toFixed(2);
     update.transactions.push("Deposit: $" + amount + " " + new Date())
       
